5cbce17c309b191d093ef267d2425bae0008a134
ThomasPotier <94831957+ThomasPotier@users.noreply.github.com>
2025-11-05 12:45:14 +0100

    maj allumage radio

--- JS/scripttst.js
+++ JS/scripttst.js
@@ -1,12 +1,48 @@
 const boutonsJaunes = document.querySelectorAll('[id^="boutonJaune"]');
 
-    boutonsJaunes.forEach(bouton => {
-      const topLine = bouton.querySelector('.top-line');
+boutonsJaunes.forEach(bouton => {
+  const topLine = bouton.querySelector('.top-line');
 
-      bouton.addEventListener('click', () => {
-        topLine.classList.add('active');
-        setTimeout(() => {
-          topLine.classList.remove('active');
-        }, 5000); // 5 secondes
-      });
-    });
+bouton.addEventListener('click', () => {
+    topLine.classList.add('active');
+    setTimeout(() => {
+      topLine.classList.remove('active');
+    }, 5000); // 5 secondes
+  });
+});
+
+// On crée une feuille CSS interne, donc même origine
+const style = document.createElement('style');
+document.head.appendChild(style);
+
+style.textContent = `
+  #sys *, #canal *, #num *, #sortie *, #annuaire *, #groupe *, #rss *, #ac *, #regul *, #volplus *, #volmoins *, #test * {
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity 0.5s ease;
+  }
+`;
+
+const powerBtn = document.querySelector('.btn-power');
+let powerOn = false; // état du système
+
+powerBtn.addEventListener('click', () => {
+  if (!powerOn) {
+   style.textContent = `
+     #sys *, #canal *, #num *, #sortie *, #annuaire *, #groupe *, #rss *, #ac *, #regul *, #volplus *, #volmoins *, #test * {
+       visibility: visible;
+       opacity: 1;
+       transition: opacity 0.5s ease;
+     }
+   `;
+ } else {
+   style.textContent = `
+     #sys *, #canal *, #num *, #sortie *, #annuaire *, #groupe *, #rss *, #ac *, #regul *, #volplus *, #volmoins *, #test *  {
+       visibility: hidden;
+       opacity: 0;
+       transition: opacity 0.5s ease;
+     }
+   `;
+ }
+ powerOn = !powerOn;
+});
